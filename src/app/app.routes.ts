7528cd7c001bb3076227a3200b8780e418c42025
aelwardi <a.elwardi98@gmail.com>
2025-04-22 19:37:04 +0200

    project ViteButNotTooMuch

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HeaderComponent } from './compoenents/header/header.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'header',
+        component: HeaderComponent
+    }
+];
